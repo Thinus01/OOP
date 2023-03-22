6d4bbd2365c34f367579b7b63f011ed031089401
Thinus <thinusvdv2001@gmail.com>
2023-03-22 15:14:52 +0200

    Added decorate, nameable, trimmer, capitalize and test .rb files

--- person.rb
+++ person.rb
@@ -1,16 +1,27 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
+  attr_reader :id
+
+  attr_accessor :name, :age
+
   def can_use_services?
-    @age >= 18 || @parent_permission
+    of_age? || @parent_permission
   end
 
   private
