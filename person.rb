b4471fa9970b1770256cf2514a8f47956052dd3f
Thinus <thinusvdv2001@gmail.com>
2023-03-23 17:35:22 +0200

    Fix output order

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(checker, age, name = 'Unknown', parent_permission: true)
+  def initialize(checker, name = 'Unknown', age, parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @checker = checker
